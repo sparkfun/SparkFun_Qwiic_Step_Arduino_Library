e06d82337685b40bdb934cadc694b2a3f92d01bb
Nathan Seidle <nathan@sparkfun.com>
2020-01-13 10:21:20 -0700

    Change and rename isReached interrupt example to use interrupt pin.

--- examples/Example5_isReachedInterrupt/Example5_isReachedInterrupt.ino
+++ examples/Example5_isReachedInterrupt/Example5_isReachedInterrupt.ino
@@ -20,22 +20,30 @@
   Distributed as-is; no warranty is given.
 ******************************************************************************/
 
-#include "SparkFun_Qwiic_Step.h"  //Click here to get the library: http://librarymanager/All#Qwiic_Step by SparkFun
+#include "SparkFun_Qwiic_Step.h" //Click here to get the library: http://librarymanager/All#Qwiic_Step by SparkFun
 QwiicStep motor;
 
-void setup(){
+#define QS_INT 2 //Any GPIO will work. Connect pin 2 on your board to the INT pin on Qwiic Step
+
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Qwiic step examples");
   Wire.begin(); //Join I2C bus
 
+  pinMode(QS_INT, INPUT_PULLUP);
+
   //check if the motor will acknowledge I2C
-  if (motor.begin() == false){
+  if (motor.begin() == false)
+  {
     Serial.println("Device did not acknowledge! Freezing.");
-    while(1); 
+    while (1)
+      ;
   }
   Serial.println("Motor acknowledged.");
 
-  motor.enablePositionReachedInterrupt();
+  motor.clearIsReached(); //Clear isReached bit. This will clear associated int.
+  motor.enableIsReachedInterrupt();
 
   //move motor
   motor.QSetMaxSpeed(800);
@@ -43,17 +51,33 @@ void setup(){
   motor.QSetAcceleration(950);
   motor.QMove(400);
 
-  //wait 3 seconds
-  //DEBUG: should see "Position reached!" and "Hello" on slave serial monitor
-  delay(3000);
+  long startTime = millis();
+  while (1)
+  {
+    if (digitalRead(QS_INT) == LOW)
+    {
+      Serial.print("Reached requested position in ");
+      Serial.print(millis() - startTime);
+      Serial.println("ms");
+      break;
+    }
 
-  //clear position reached interrupt flag
-  //motor status should still be "isReached" but interrupt pin should go high
-  //DEBUG: should only see "Position reached!" on slave serial monitor
-  Serial.println("Hello, I'm clearing the interrupt flag");
-  motor.clearIsReachedInterrupt();
-} 
+    delay(10);
 
-void loop(){
-  
+    if (millis() - startTime > 3000) //Timeout after 3s
+    {
+      Serial.println("Interrupt did not trigger. Do you have the correct interrupt pin wired?");
+      break;
+    }
+  }
+
+  motor.clearIsReached(); //Clear isReached bit. This will clear associated int.
+  if (digitalRead(QS_INT) == HIGH)
+    Serial.println("INT pin has returned high. Interrupt cleared.");
+  else
+    Serial.println("INT pin is still low. Interrupt failed to clear.");
+}
+
+void loop()
+{
 }
